3d71ba5746bf0f74317aa5e239ac1a704606f24e
Hubert Wójcik <hwojcikk@gmail.com>
2024-11-28 07:30:43 +0100

    calculate most common

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from collections import defaultdict
 
 users = [
     {"id":0,"name":"Hero"},
@@ -60,4 +61,47 @@ def friends_of_friend_ids(user):
         and foaf_id not in friendships[user_id]
     )
 
-print(friends_of_friend_ids(users[3]))
+# Interests
+interests = [
+    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
+    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
+    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
+    (2, "Postgres"), (2, "Python"), (2, "scikit-learn"),
+    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), 
+    (3, "Python"), (3, "statistics"), (3, "regression"), (3, "probability"),
+    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
+    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
+    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
+    (6, "probability"), (6, "mathematics"), (6, "decision trees"),
+    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
+    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
+    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
+    (9, "Java"), (9, "MapReduce"), (9, "Big Data")
+]
+
+
+def data_scientists_who_like(target_interest):
+    return [
+        user_id
+        for user_id, user_interest in interests
+        if user_interest == target_interest
+    ]
+
+user_ids_by_interest = defaultdict(list)
+
+for user_id, interest in interests:
+    user_ids_by_interest[interest].append(user_id)
+
+interests_by_user_id = defaultdict(list)
+
+for user_id, interest in interests:
+    interests_by_user_id[user_id].append(interest)
+
+def most_common_interests_with(user):
+    return Counter(
+        interested_user_id
+        for interest in interests_by_user_id[user["id"]]
+        for interested_user_id in user_ids_by_interest[interest]
+        if interested_user_id != user["id"]
+    )
+
